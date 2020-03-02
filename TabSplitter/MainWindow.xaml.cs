de719a8d2b526cc0a77ab65842a03a03ab117c81
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2020-03-02 18:15:07 +0530

    Added TabSplitter sample

---
+++ TabSplitter/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Syncfusion.Windows.Tools.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TabSplitter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Syncfusion.Windows.Tools.Controls.TabSplitter tabSplitter = new Syncfusion.Windows.Tools.Controls.TabSplitter();
+            //tabSplitter.Height = 280;
+            //tabSplitter.Width = 400;
+
+            //SplitterPage splitterPage = new SplitterPage();
+            //splitterPage.Header = "XAML";
+            //SplitterPage splitterPage1 = new SplitterPage();
+            //splitterPage1.Header = "Design";
+            
+
+            //TabSplitterItem tabSplitterItem1 = new TabSplitterItem();
+            //tabSplitterItem1.Header = "MainWindow.XAML";
+
+            //tabSplitterItem1.TopPanelItems.Add(splitterPage);
+            //tabSplitterItem1.BottomPanelItems.Add(splitterPage1);
+            ////tabSplitterItem1.Orientation = Orientation.Vertical;
+            ////TabSplitterItem tabSplitterItem2 = new TabSplitterItem();
+            ////tabSplitterItem2.Header = "Design";
+            ////tabSplitterItem2.BottomPanelItems.Add(splitterPage);
+            
+            //Label label = new Label();
+            //label.Content = "XAML";
+            //label.HorizontalContentAlignment = HorizontalAlignment.Center;
+            //label.VerticalContentAlignment = VerticalAlignment.Center;
+
+            //Label label1 = new Label();
+            //label1.Content = "Design";
+            //label1.HorizontalContentAlignment = HorizontalAlignment.Center;
+            //label1.VerticalContentAlignment = VerticalAlignment.Center;
+
+            //splitterPage.Content = label;
+            //splitterPage1.Content = label1;
+            //tabSplitter.Items.Add(tabSplitterItem1);
+
+            ////tabSplitterItem1.IsCollapsedBottomPanel = true;
+
+            ////tabSplitter.Items.Add(tabSplitterItem2);
+            //grid.Children.Add(tabSplitter);
+        }
+    }
+}
