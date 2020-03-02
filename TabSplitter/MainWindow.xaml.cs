1672ad3ff37d6c449c89393c93d856fafef3ffee
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2020-03-02 18:17:12 +0530

    Modified the sample

--- TabSplitter/MainWindow.xaml.cs
+++ TabSplitter/MainWindow.xaml.cs
@@ -23,45 +23,7 @@ namespace TabSplitter
     {
         public MainWindow()
         {
-            InitializeComponent();
-            //Syncfusion.Windows.Tools.Controls.TabSplitter tabSplitter = new Syncfusion.Windows.Tools.Controls.TabSplitter();
-            //tabSplitter.Height = 280;
-            //tabSplitter.Width = 400;
-
-            //SplitterPage splitterPage = new SplitterPage();
-            //splitterPage.Header = "XAML";
-            //SplitterPage splitterPage1 = new SplitterPage();
-            //splitterPage1.Header = "Design";
-            
-
-            //TabSplitterItem tabSplitterItem1 = new TabSplitterItem();
-            //tabSplitterItem1.Header = "MainWindow.XAML";
-
-            //tabSplitterItem1.TopPanelItems.Add(splitterPage);
-            //tabSplitterItem1.BottomPanelItems.Add(splitterPage1);
-            ////tabSplitterItem1.Orientation = Orientation.Vertical;
-            ////TabSplitterItem tabSplitterItem2 = new TabSplitterItem();
-            ////tabSplitterItem2.Header = "Design";
-            ////tabSplitterItem2.BottomPanelItems.Add(splitterPage);
-            
-            //Label label = new Label();
-            //label.Content = "XAML";
-            //label.HorizontalContentAlignment = HorizontalAlignment.Center;
-            //label.VerticalContentAlignment = VerticalAlignment.Center;
-
-            //Label label1 = new Label();
-            //label1.Content = "Design";
-            //label1.HorizontalContentAlignment = HorizontalAlignment.Center;
-            //label1.VerticalContentAlignment = VerticalAlignment.Center;
-
-            //splitterPage.Content = label;
-            //splitterPage1.Content = label1;
-            //tabSplitter.Items.Add(tabSplitterItem1);
-
-            ////tabSplitterItem1.IsCollapsedBottomPanel = true;
-
-            ////tabSplitter.Items.Add(tabSplitterItem2);
-            //grid.Children.Add(tabSplitter);
+            InitializeComponent();          
         }
     }
 }
